7f295e30224f4ceb4c3ac8928ef1dfe6842a00ee
Angelica <Angelica@ANGELICA>
2023-10-25 21:07:18 -0700

    Add project files.

---
+++ Streaming/index.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Streaming.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Cargar Videos</title>
+
+    <h1 style="font-family:Arial; text-align:center">Cargar Videos</h1>
+    <style>
+        iframe {
+            height: 400px;
+        }
+     </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+           <label>Nombre del video</label> <asp:TextBox ID="txtNombre"  runat="server"></asp:TextBox><br>
+           <label>Descripcion</label> <asp:TextBox ID="txtDescripcion" runat="server"></asp:TextBox><br>
+            <asp:FileUpload ID="fuVideo" runat="server" /><br>
+            <asp:Button ID="btnCargar" runat="server" Text="cargar" OnClick="btnCargar_Click" />
+            <br>
+            <label>URL</label><asp:TextBox ID="txtURL" runat="server"></asp:TextBox>
+            <asp:Literal ID="lit" runat="server"></asp:Literal>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Streaming/index.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ApiVideo.Api;
+using ApiVideo.Client;
+using ApiVideo.Model;
+
+
+namespace Streaming
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCargar_Click(object sender, EventArgs e)
+        {
+            var apiKey = "SjaEhidlsHHznZHAw0efMf6TDUZpsBy335FlwuUQ79v";
+
+            var apiVideoClient = new ApiVideoClient(apiKey);
+            
+            var videoPayload = new VideoCreationPayload()
+            {
+                title = txtNombre.Text,
+                description = txtDescripcion.Text,
+                mp4support = true,
+                tags = new List<string>()
+            {
+                "first","video","test","c#","client"
+            }
+            };
+            Stream myVideoFile = fuVideo.FileContent;
+           
+            try
+            {
+                var newVideo = apiVideoClient.Videos().create(videoPayload);
+                var video = apiVideoClient.Videos().upload(newVideo.videoid, myVideoFile);
+
+                txtURL.Text = video.assets.player;
+                lit.Text = video.assets.iframe;
+                
+            }
+            catch (ApiException ex)
+            {
+                //Console.WriteLine(e.ErrorCode);
+                //Console.WriteLine(e.Message);
+                //Console.WriteLine(e.StackTrace);
+            }
+
+        }
+}
+}
